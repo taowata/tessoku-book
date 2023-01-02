e925f213b7cf3c1fdbf2a7cd73398f1505867412
taowata <momossy5@gmail.com>
2023-01-02 17:36:47 +0900

    A05

---
+++ A/A05/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+var sc = bufio.NewScanner(os.Stdin)
+
+func scanInt() int {
+	sc.Scan()
+	i, err := strconv.Atoi(sc.Text())
+	if err != nil {
+		panic(err)
+	}
+	return i
+}
+
+func main() {
+	sc.Split(bufio.ScanWords)
+
+	N, K := scanInt(), scanInt()
+	answer := 0
+	for i := 1; i <= N; i++ {
+		for j := 1; j <= N; j++ {
+			k := K - i - j
+			if k >= 1 && k <= N {
+				answer += 1
+			}
+		}
+	}
+	fmt.Println(answer)
+}
